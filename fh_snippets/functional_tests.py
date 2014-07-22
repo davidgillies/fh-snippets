090d96d477bf35dabf1cfc5e3201cb8271ba5477
David Gillies <david.gillies.rhu@gmail.com>
2014-07-22 16:55:34 +0100

    binned old settings, set up functional tests

--- fh_snippets/functional_tests.py
+++ fh_snippets/functional_tests.py
@@ -1,7 +1,20 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3) # givess FF response time
+    def tearDown(self):
+        self.browser.quit()
 
-assert 'Django' in browser.title
+    def test_dashboard_in_title(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Dashboard', self.browser.title)
+        # self.fail('Finished') # in to make sure test fails til you finished it.
+
+if __name__ == '__main__':
+    unittest.main()
+
+
 
