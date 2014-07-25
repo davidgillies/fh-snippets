ce7f2a602147e329f909d377c3a68d29b82abebf
David Gillies <david.gillies.rhu@gmail.com>
2014-07-25 14:21:55 +0100

    added a basic tree model, needs revising

--- fh_snippets/tree/admin.py
+++ fh_snippets/tree/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from tree.models import Tree
 # Register your models here.
+
+class TreeAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Tree, TreeAdmin)
--- fh_snippets/tree/models.py
+++ fh_snippets/tree/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from tags.models import Tag
 
 # Create your models here.
+
+class Tree(models.Model):
+    first_name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=40)
+    birth_date = models.CharField(max_length=10, blank=True)
+    death_date = models.CharField(max_length=10, blank=True)
+    marriages = models.ManyToManyField('self', blank=True, null=True)
+    #parents = models.ForeignKey('self', blank=True, null=True)
+    notes = models.TextField(blank=True)
+    tags = models.ManyToManyField(Tag, blank=True, null=True)
+
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.surname)
+
+    class Meta:
+        ordering = ('surname','birth_date',)
