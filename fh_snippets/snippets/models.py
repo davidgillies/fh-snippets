5e0bdeaeeeeff1ef9efb259d372a914f3ffa70d9
David Gillies <david.gillies.rhu@gmail.com>
2014-07-25 13:47:17 +0100

    added in snippets/tags models and also set them up for admin

--- fh_snippets/snippets/models.py
+++ fh_snippets/snippets/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from tags.models import Tag
 
 # Create your models here.
+
+class Snippet(models.Model):
+    BOOK = 'BK'
+    MAP = 'MP'
+    IMAGE = 'IMG'
+    SOURCE_TYPES = (
+        (BOOK, 'Book'),
+        (MAP, 'Map'),
+        (IMAGE, 'Image'),
+    )
+    
+    snippet = models.TextField()
+    source_title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    source_type = models.CharField(max_length=10, choices=SOURCE_TYPES,default=BOOK)
+    notes = models.TextField()
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.author
+
+    class Meta:
+        ordering = ('author',)
+
+
--- fh_snippets/tags/models.py
+++ fh_snippets/tags/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+    OCCUPATION = 'occ'
+    LOCATION = 'loc'
+    PERIOD = 'per'
+    PERSON = 'ppe'
+    TAG_TYPE_CHOICES = (
+        (OCCUPATION, 'Occupation'),
+        (LOCATION, 'location'),
+        (PERIOD, 'Period'),
+        (PERSON, 'Person'),
+    )
+
+    tagname = models.CharField(max_length=200)
+    description = models.TextField()
+    tag_type = models.CharField(max_length=10, choices=TAG_TYPE_CHOICES)
+
+    def __str__(self):
+        return self.tagname
+
+    class Meta:
+        ordering = ('tag_type',)
+
