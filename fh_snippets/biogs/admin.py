77ef9f028893198aeadb0b53029fa3a969fa436f
David Gillies <david.gillies.rhu@gmail.com>
2014-07-27 10:00:43 +0100

    added in biogs models

--- fh_snippets/biogs/admin.py
+++ fh_snippets/biogs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from biogs.models import Biog
 # Register your models here.
+
+class BiogAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Biog, BiogAdmin)
--- fh_snippets/biogs/models.py
+++ fh_snippets/biogs/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from tree.models import Tree, Family
+from tags.models import Tag
 # Create your models here.
+
+class Biog(models.Model):
+    first_name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=40)
+    birth_year = models.CharField(max_length=4)
+    tags = models.ManyToManyField(Tag, blank=True, null=True)
+    tree_members = models.ManyToManyField(Tree, blank=True, null=True)    
+    notes = models.TextField(blank=True)
+    
+    def __str__(self):
+        return "%s, %s" % (self.surname, self.first_name)
+
+    class Meta:
+        ordering = ('surname', 'first_name', 'birth_year',)
+
