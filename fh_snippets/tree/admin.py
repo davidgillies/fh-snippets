34d227a6c39978cd2d1c6f10ece81a245fd1771d
David Gillies <david.gillies.rhu@gmail.com>
2014-07-27 09:35:58 +0100

    redesign of tree models based on gedcom

--- fh_snippets/tree/admin.py
+++ fh_snippets/tree/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from tree.models import Tree
+from tree.models import Tree, Family
 # Register your models here.
 
 class TreeAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Tree, TreeAdmin)
+
+class FamilyAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Family, admin.ModelAdmin)
+
--- fh_snippets/tree/models.py
+++ fh_snippets/tree/models.py
@@ -8,13 +8,22 @@ class Tree(models.Model):
     surname = models.CharField(max_length=40)
     birth_date = models.CharField(max_length=10, blank=True)
     death_date = models.CharField(max_length=10, blank=True)
-    marriages = models.ManyToManyField('self', blank=True, null=True)
-    parents = models.ManyToManyField('self', symmetrical=False, blank=True, null=True)
     notes = models.TextField(blank=True)
     tags = models.ManyToManyField(Tag, blank=True, null=True)
-
     def __str__(self):
         return "%s %s" % (self.first_name, self.surname)
 
     class Meta:
         ordering = ('surname','birth_date',)
+
+class Family(models.Model):
+    husband = models.ForeignKey(Tree, related_name='hubbie')
+    wife = models.ForeignKey(Tree, related_name='wiffy')
+    marriage_date = models.CharField(max_length=20)
+    marriage_place = models.CharField(max_length=50)
+    children = models.ManyToManyField(Tree, related_name='kids')
+    
+    def __str__(self):
+        return "%s, %s" % (self.husband.surname, self.wife.surname)
+    
+    
